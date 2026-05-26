b9221ac9512578c7f3d4a4a468cbe32df4e0b622
zaber <20830726+ZaberKo@users.noreply.github.com>
2026-05-26 20:07:27 +0800

    update cfg

--- scripts/atari_new_stage1_train_policy_supernet.sh
+++ scripts/atari_new_stage1_train_policy_supernet.sh
@@ -6,7 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 python new_stage1_train_policy_supernet.py \
   --ppo_config config.yaml \
   --ppo_config_override ppo.n_epochs=4 \
-  --ppo_config_override ppo.total_timesteps=20000000 \
+  --ppo_config_override ppo.total_timesteps=30000000 \
   --ppo_config_override ppo.z_dyn_coef=0.01 \
   --ppo_config_override ppo.ema_tau=0.99 \
   --ppo_config_override ppo.projection_dim=128 \
